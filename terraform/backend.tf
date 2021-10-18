19bb70c0389fcb3ac3199088cdf5698ff774cdb9
Nathan Mills <rivernate@gmail.com>
2021-10-17 20:14:14 -0600

    migrate backend to tf cloud

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,10 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform.rivernate.com"
-        key = "rivernate.com/root_domain"
-        region = "us-west-2"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "rivernate"
+
+    workspaces {
+      name = "rivernatecom"
     }
+  }
 }
